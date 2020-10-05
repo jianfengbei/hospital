0b6c316facb3dc6d726b7f15c5bd0c1ba0ddfe85
jianfengbei <jianfengbei@gmail.com>
2020-10-05 11:08:19 +0800

    Add files via upload

---
+++ hospital_Q&A.sql
@@ -0,0 +1,343 @@
+
+USE `hospital`;
+/* 1. Write a query in SQL to find all the information of the nurses 
+who are yet to be registered.*/
+SELECT * FROM `nurse`
+WHERE `registered` = 'f';
+
+CALL `hospital`.`registered_nurse`();
+
+
+/* 2. Write a query in SQL to find the name of the nurse who are the head of their department.*/
+SELECT `name` 
+FROM `nurse`
+WHERE `position` LIKE 'HEAD%';
+
+
+/* 3. Write a query in SQL to obtain the name of the physicians who are the head of each department.*/
+SELECT `name` FROM `physician`
+WHERE `employeeid` IN (SELECT `head` FROM `department`);
+
+/* 4.Write a query in SQL to count the number of patients who taken appointment with at least one physician. */
+SELECT `patient`, COUNT(`patient`) patient_pop
+FROM `appointment`
+WHERE `physician` IS NOT NULL
+GROUP BY `patient`;
+
+/* 5.Write a query in SQL to find the floor and block where the room number 212 belongs to.*/
+SELECT blockfloor, blockcode
+FROM room
+where roomnumber = 212;
+
+CALL `hospital`.`find_room`(103);
+
+
+
+/* 6.Write a query in SQL to count the number available rooms.*/
+SELECT COUNT(unavailable) Room_available
+FROM room
+WHERE unavailable = 'f';
+
+CALL `hospital`.`room_available`();
+
+SELECT `hospital`.`available_room`() FROM DUAL;
+
+/* 7. Write a query in SQL to count the number of unavailable rooms.  */
+SELECT COUNT(unavailable)
+FROM room
+WHERE unavailable = 't';
+
+
+/* 8. Write a query in SQL to obtain the name of the physician and the departments they are affiliated with.*/
+SELECT phy.name physician, dept.name department
+FROM physician phy, department dept, affiliated_with afw
+WHERE phy.employeeid = afw.physician
+AND dept.departmentid = afw.department
+AND afw.primaryaffiliation = 't';
+
+CALL `hospital`.`physician_affiliation`(8);
+
+
+/* 9. Write a query in SQL to obtain the name of the physicians who are trained for a special treatement.*/
+SELECT phy.name, PRO.NAME TREATMENT_NAME
+FROM physician phy, trained_in tin,`PROCEDURE` PRO
+WHERE phy.employeeid=tin.physician
+AND tin.treatment = pro.code;
+
+CALL `hospital`.`physician_training`(3);
+
+/*mysql has a bug */
+CREATE DEFINER=`admin`@`localhost` PROCEDURE `physician_training`()
+BEGIN
+  DECLARE v_name varchar(20);
+  DECLARE v_treatment VARCHAR(50);
+  DECLARE done INT DEFAULT FALSE;
+  DECLARE c1 cursor for 
+  SELECT TT.* FROM (SELECT phy.name, PRO.NAME TREATMENT_NAME
+                 FROM physician phy, trained_in tin,`PROCEDURE` PRO
+                 WHERE phy.employeeid=tin.physician
+                 AND tin.treatment = pro.code
+                 AND phy.employeeid = employee_id) TT;
+  DECLARE continue handler for NOT FOUND set done = TRUE;
+  open c1;
+  read_loop:LOOP
+     fetch c1 into v_name, v_treatment;
+     if v_finished = TRUE then
+       leave physician_training;
+	 end if;
+     select concat(v_name,v_treatment);
+     END LOOP;
+  close c1;
+END
+
+
+/* 10. Write a query in SQL to obtain the name of the physicians with department who are yet to be affiliated.*/
+SELECT phy.name, dept.name
+FROM physician phy, department dept, affiliated_with afw
+WHERE phy.employeeid = afw.physician
+AND dept.departmentid = afw.department
+AND afw.primaryaffiliation = 'f';
+
+
+/* 11. Write a query in SQL to obtain the name of the physicians who are not a specialized physician.*/
+SELECT *
+FROM physician
+WHERE position NOT LIKE '%Physician';
+
+/* 12. Write a query in SQL to obtain the name of the patients with their physicians by whom they got their preliminary treatement.*/
+SELECT patient.name, physician.name
+FROM patient, physician
+WHERE patient.pcp = physician.employeeid;
+
+/* 13. Write a query in SQL to find the name of the patients and the number of physicians they have taken appointment.*/
+
+SELECT name, count(physician) phy_nb
+FROM (SELECT patient.name, appointment.physician, ROW_NUMBER() OVER (PARTITION BY name, physician) NB
+      FROM patient, appointment
+      WHERE patient.ssn = appointment.patient
+      ORDER BY patient.name, appointment.physician) AA
+WHERE NB = 1
+GROUP BY name;
+
+/* 14. Write a query in SQL to count number of unique patients who got an appointment for examination room C.*/
+
+SELECT COUNT(DISTINCT patient)
+FROM appointment
+WHERE examinationroom = 'C';
+
+/* 15. Write a query in SQL to find the name of the patients and the number of the room where they have to go for their treatment.*/
+SELECT name, examinationroom
+FROM patient, appointment
+WHERE `patient`.ssn = appointment.patient
+ORDER BY name;
+
+/* 16. Write a query in SQL to find the name of the nurses and the room scheduled, where they will assist the physicians. */
+SELECT nurse.name, examinationroom
+FROM nurse, appointment
+WHERE nurse.employeeid = appointment.prepnurse;
+
+/* 17. Write a query in SQL to find the name of the patients who taken the appointment on the 25th of April at 10 am, and also display their physician, assisting nurses and room no.*/
+SELECT patient.name, physician, prepnurse, examinationroom, start_dt_time
+FROM patient, appointment,physician, nurse
+WHERE physician.employeeid = appointment.physician
+AND nurse.employeeid=appointment.prepnurse
+AND start_dt_time = CONVERT('2008-04-25 10:00:00',DATETIME);
+
+/* 18. Write a query in SQL to find the name of patients and their physicians who does not require any assistance of a nurse. */
+SELECT patient.name, physician.name
+FROM patient, appointment, physician
+WHERE patient.ssn = appointment.patient
+AND appointment.physician = physician.employeeid
+AND appointment.prepnurse IS NULL;
+
+/* 19. Write a query in SQL to find the name of the patients, their treating physicians and medication.*/
+SELECT patient.name patient_name,physician.name physician_name, medication.name medication_name
+FROM patient,physician, prescribes, medication
+WHERE patient.ssn=prescribes.patient
+AND physician.employeeid = prescribes.physician
+AND medication.code = prescribes.medication;
+
+
+/* 20. Write a query in SQL to find the name of the patients who taken an advanced appointment, and also display their physicians and medication.*/
+SELECT patient.name, physician.name, medication.name
+FROM patient, prescribes, physician, medication
+WHERE patient.ssn = prescribes.patient
+AND appointment IS NOT NULL
+AND physician.employeeid = prescribes.physician
+AND medication.code = prescribes.medication;
+
+
+/* 21. Write a query in SQL to find the name and medication for those patients who did not take any appointment.*/
+SELECT patient.name, physician.name, medication.name
+FROM patient, prescribes, physician, medication
+WHERE patient.ssn = prescribes.patient
+AND appointment IS NULL
+AND physician.employeeid = prescribes.physician
+AND medication.code = prescribes.medication;
+
+
+/* 22. Write a query in SQL to count the number of available rooms in each block.*/
+SELECT blockcode, count(unavailable) room_num
+FROM ROOM
+WHERE unavailable='f'
+GROUP BY blockcode;
+
+
+/* 23. Write a query in SQL to count the number of available rooms in each floor. */
+SELECT blockfloor, count(unavailable) room_num
+FROM ROOM
+WHERE unavailable='f'
+GROUP BY blockfloor;
+
+
+/* 24. Write a query in SQL to count the number of available rooms for each block in each floor. */
+SELECT blockcode, blockfloor, count(unavailable) room_num
+FROM ROOM
+WHERE unavailable='f'
+GROUP BY blockcode, blockfloor;
+
+
+/* 25. Write a query in SQL to count the number of unavailable rooms for each block in each floor.*/
+SELECT blockcode, blockfloor, count(unavailable) room_num
+FROM ROOM
+WHERE unavailable='t'
+GROUP BY blockcode, blockfloor;
+
+
+/* 26. Write a query in SQL to find out the floor where the maximum no of rooms are available.*/
+
+SELECT blockfloor,count(unavailable) room_num
+FROM ROOM
+WHERE unavailable='f'
+GROUP BY blockfloor
+ORDER BY room_num DESC
+LIMIT 1;
+
+
+/* 27. Write a query in SQL to find out the floor where the minimum no of rooms are available.*/
+SELECT blockfloor,count(unavailable) room_num
+FROM ROOM
+WHERE unavailable='f'
+GROUP BY blockfloor
+ORDER BY room_num
+LIMIT 1
+
+/* 28. Write a query in SQL to obtain the name of the patients, their block, floor, and room number where they are admitted. */
+SELECT patient.name, stay.room,room.blockfloor,room.blockcode
+FROM patient, stay, room
+WHERE patient.ssn = stay.patient
+AND room.roomnumber = stay.room;
+
+/* 29. Write a query in SQL to obtain the nurses and the block where they are booked for attending the patients on call.*/
+SELECT nurse.name,blockcode
+FROM nurse, on_call
+WHERE nurse.employeeid = on_call.nurse;
+
+/* 30. Write a query in SQL to make a report which will show -
+a) name of the patient, 
+b) name of the physician who is treating him or her,
+c) name of the nurse who is attending him or her,
+d) which treatement is going on to the patient,
+e) the date of release,
+f) in which room the patient has admitted and which floor and block the room belongs to respectively.*/
+
+SELECT patient.name patient_name,physician.name physician_name,nurse.name nurse_name,'procedure',stay.end_time,room.roomnumber,room.blockfloor,room.blockcode
+FROM patient, physician, nurse, undergoes, stay,room
+WHERE patient.ssn=undergoes.patient
+AND physician.employeeid = undergoes.physician
+AND nurse.employeeid = undergoes.assistingnurse
+AND stay.patient = undergoes.patient
+AND room.roomnumber = stay.room;
+
+/* 31. Write a SQL query to obtain the names of all the physicians performed a medical procedure but they are not ceritifed to perform. */
+SELECT TT.name,`procedure`,position
+FROM (SELECT name, `procedure`, position
+      FROM physician, undergoes
+      WHERE physician.employeeid = undergoes.physician) TT
+LEFT JOIN (SELECT name,treatment
+           FROM physician, trained_in
+           WHERE trained_in.physician = physician.employeeid) BB
+ON TT.name = BB.name AND TT.`procedure` = BB.treatment
+WHERE BB.name is null;
+
+
+/* 32. Write a query in SQL to obtain the names of all the physicians, their procedure, date when the procedure was carried out and name of the patient on which procedure have been carried out but those physicians are not cetified for that procedure. */
+SELECT physician,procedure_name, date, patient
+FROM (SELECT physician.name physician, physician.employeeid ID, undergoes.procedure, undergoes.date,patient.name patient,`procedure`.name procedure_name
+FROM physician, undergoes,patient, `procedure`
+WHERE physician.employeeid = undergoes.physician
+AND patient.ssn=undergoes.patient
+AND `procedure`.code = undergoes.`procedure`) AA
+
+LEFT JOIN (SELECT employeeid,treatment
+           FROM physician, trained_in
+           WHERE trained_in.physician = physician.employeeid) BB
+ON AA.ID = BB.employeeid AND AA.procedure = BB.treatment
+WHERE BB.employeeid is null;
+
+
+/* 33. Write a query in SQL to obtain the name and position of all physicians who completed a medical procedure with certification after the date of expiration of their certificate. */
+SELECT name, position
+FROM physician, (SELECT undergoes.physician, `procedure`
+FROM undergoes, trained_in
+WHERE undergoes.`procedure` = trained_in.treatment
+AND undergoes.date > certificationexpires) AA
+WHERE physician.employeeid = AA.physician;
+
+/* 34. Write a query in SQL to obtain the name of all those physicians who completed a medical procedure with certification after the date of expiration of their certificate, their position, procedure they have done, date of procedure, name of the patient on which the procedure had been applied and the date when the certification expired*/
+SELECT physician.name, position, `procedure`.name procedure_name,AA.procedure_date, patient.name patient_name, AA.expire_date
+FROM physician, (SELECT undergoes.physician, undergoes.patient, `procedure`, undergoes.date procedure_date, certificationexpires expire_date
+				 FROM undergoes, trained_in
+				 WHERE undergoes.`procedure` = trained_in.treatment
+				 AND undergoes.date > certificationexpires) AA, 
+                 `procedure`, patient
+WHERE physician.employeeid = AA.physician
+AND `procedure`.code = AA.`procedure`
+AND patient.ssn = AA.patient;
+
+/* 35. Write a query in SQL to obtain the names of all the nurses who have ever been on call for room 122. */
+SELECT name
+FROM nurse, on_call,room
+WHERE nurse.employeeid = on_call.nurse
+AND on_call.blockfloor = room.blockfloor
+AND on_call.blockcode = room.blockcode
+AND room.roomnumber = 122;
+
+/* 36. Write a query in SQL to Obtain the names of all patients who has been prescribed some medication by his/her physician who has carried out primary care and the name of that physician.*/
+
+SELECT patient.name
+FROM patient, prescribes, physician
+WHERE patient.ssn = prescribes.patient
+AND patient.pcp=prescribes.physician
+AND physician.employeeid = patient.pcp;
+
+
+/* 37. Write a query in SQL to obtain the names of all patients who has been undergone a procedure costing more than $5,000 and the name of that physician who has carried out primary care. */
+SELECT patient.name patient_name, physician.name physician_name
+FROM patient, undergoes,physician, `procedure`
+WHERE patient.ssn=undergoes.patient
+AND patient.pcp=physician.employeeid
+AND undergoes.`procedure` = `procedure`.code
+AND `procedure`.cost > 5000;
+
+/* 38. Write a query in SQL to Obtain the names of all patients who had at least two appointment where the nurse who prepped the appointment was a registered nurse and the physician who has carried out primary care.*/
+SELECT DISTINCT patient_name,nurse_name,physician_name
+FROM (
+SELECT patient.name patient_name,nurse.name nurse_name, physician.name physician_name,
+       COUNT(appointmentid) OVER (PARTITION BY patient.name) num_appointment
+FROM appointment,patient,nurse, physician
+WHERE appointment.patient = patient.ssn
+AND appointment.prepnurse = nurse.employeeid
+AND physician.employeeid = patient.pcp
+AND nurse.registered = 't') TT
+WHERE num_appointment >=2;
+
+
+/* 39. Write a query in SQL to Obtain the names of all patients whose primary care is taken by a physician who is not the head of any department and name of that physician along with their primary care physician.*/
+SELECT patient.name patient, physician.name physician
+FROM patient, physician
+WHERE patient.pcp = physician.employeeid
+AND pcp NOT IN (SELECT head FROM department);
+
+
+
---
+++ hospital_database.sql
@@ -0,0 +1,341 @@
+/* hospital database
+
+Table list:
+department,room;
+physician, procedure, trained_in,affiliated_with;
+nurse, on_call;
+patient,appointment,stay,prescribes, medication,undergoes.
+
+ER diagram: conducted
+
+
+source: https://www.w3resource.com/sql-exercises/hospital-database-exercise/sql-exercise-on-hospital-database.php#h_one
+
+ */
+DROP DATABASE IF EXISTS `hospital`;
+CREATE DATABASE `hospital`;
+
+USE `hospital`;
+
+/* table structure for: nurse*/
+DROP TABLE IF EXISTS `nurse`;
+CREATE TABLE `nurse` (
+`employeeid` INT PRIMARY KEY,
+`name` VARCHAR(20),
+`position` VARCHAR(20),
+`registered` ENUM('f','t'),
+`ssn` INT
+);
+INSERT INTO `nurse` (`employeeid`,`name`,`position`,`registered`,`ssn`) VALUES
+(101, 'Carla Espinosa', 'Head Nurse','t',111111110),
+(102, 'Laverne Roberts', 'Nurse','t',222222220),
+(103, 'Paul Flowers', 'Nurse', 'f', 333333330);
+
+
+/* Table structure for: physician*/
+DROP TABLE IF EXISTS `physician`;
+CREATE TABLE `physician` (
+`employeeid` INT PRIMARY KEY,
+`name` varchar(20),
+`position` varchar(50),
+`ssn` INT
+);
+INSERT INTO `physician` (`employeeid`, `name`, `position`, `ssn`) VALUES
+(1, 'John Dorian', 'Staff Internist', 111111111),
+(2,'Elliot Reid', 'Attending Physician', 222222222),
+(3,'Christopher Turk', 'Surgical Attending Physician', 333333333),
+(4, 'Percival Cox', 'Senior Attending Physician', 444444444),
+(5, 'Bob Kelso', 'Head Chief of Medicine', 555555555),
+(6, 'Todd Quinlan', 'Surgical Attending Physician', 666666666),
+(7, 'John Wen', 'Surgical Attending Physician', 777777777), 
+(8, 'Keith Dudemeister', 'MD Resident', 888888888), 
+(9, 'Molly Clock', 'Attending Psychiatrist', 999999999);
+
+/* Table structure for: department*/
+DROP TABLE IF EXISTS `department`;
+CREATE TABLE `department` (
+`departmentid` INT PRIMARY KEY,
+`name` varchar(20),
+`head` INT
+);
+INSERT INTO `department` (`departmentid`,`name`,`head`) VALUES
+(1, 'General Medicine', 4),
+(2, 'Surgery', 7),
+(3, 'Psychiatry', 9);
+
+/* Table structure for: appointment */
+DROP TABLE IF EXISTS `appointment`;
+CREATE TABLE `appointment` (
+  `appointmentid` INT PRIMARY KEY,
+  `patient` INT,
+  `prepnurse` INT, 
+  `physician` INT,
+  `start_dt_time` DATETIME,
+  `end_dt_time` DATETIME,
+  `examinationroom` VARCHAR(1)
+);
+INSERT INTO `appointment` (`appointmentid`,`patient`,`prepnurse`,`physician`,`start_dt_time`,`end_dt_time`,`examinationroom`) VALUES
+(13216584,100000001,101,1,'2008-04-24 10:00:00','2008-04-24 11:00:00','A'),
+(26548913,100000002,101,2,'2008-04-24 10:00:00','2008-04-24 11:00:00','B'),
+(36549879,100000001,102,1,'2008-04-25 10:00:00','2008-04-25 11:00:00','A'),
+(46846589,100000004,103,4,'2008-04-25 10:00:00','2008-04-25 11:00:00','B'),
+(59871321,100000004,null,4,'2008-04-26 10:00:00','2008-04-26 11:00:00','C'),
+(69879231,100000003,103,2,'2008-04-26 11:00:00','2008-04-26 12:00:00','C'),
+(76983231,100000001,null,3,'2008-04-26 12:00:00','2008-04-26 13:00:00','C'),
+(86213939,100000004,102,9,'2008-04-27 10:00:00','2008-04-21 11:00:00','A'),
+(93216548,100000002,101,2,'2008-04-27 10:00:00','2008-04-27 11:00:00','B');
+
+/* Table structure for: room*/
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
+`roomnumber` INT PRIMARY KEY,
+`roomtype` VARCHAR(8),
+`blockfloor` INT,
+`blockcode` INT,
+`unavailable` ENUM('f','t'));
+INSERT INTO `room` (`roomnumber`,`roomtype`,`blockfloor`,`blockcode`,`unavailable`) VALUES
+(101,'Single',1,1,'f'),
+(102,'Single',1,1,'f'),
+(103,'Single',1,1,'f'),
+(111,'Single',1,2,'f'),
+(112,'Single',1,2,'t'),
+(113,'Single',1,2,'f'),
+(121,'Single',1,3,'f'),
+(122,'Single',1,3,'f'),
+(123,'Single',1,3,'f'),
+(201,'Single',2,1,'t'),
+(202,'Single',2,1,'f'),
+(203,'Single',2,1,'f'),
+(211,'Single',2,2,'f'),
+(212,'Single',2,2,'f'),
+(213,'Single',2,2,'t'),
+(221,'Single',2,3,'f'),
+(222,'Single',2,3,'f'),
+(223,'Single',2,3,'f'),
+(301,'Single',3,1,'f'),
+(302,'Single',3,1,'t'),
+(303,'Single',3,1,'f'),
+(311,'Single',3,2,'f'),
+(312,'Single',3,2,'f'),
+(313,'Single',3,2,'f'),
+(321,'Single',3,3,'t'),
+(322,'Single',3,3,'f'),
+(323,'Single',3,3,'f'),
+(401,'Single',4,1,'f'),
+(402,'Single',4,1,'t'),
+(403,'Single',4,1,'f'),
+(411,'Single',4,2,'f'),
+(412,'Single',4,2,'f'),
+(413,'Single',4,2,'f'),
+(421,'Single',4,3,'t'),
+(422,'Single',4,3,'f'),
+(423,'Single',4,3,'f');
+
+/* Table structure for: affiliated_with*/
+DROP TABLE IF EXISTS `affiliated_with`;
+CREATE TABLE `affiliated_with`(
+`physician` INT,
+`department` INT,
+`primaryaffiliation` ENUM('t','f')
+);
+INSERT INTO `affiliated_with`(`physician`,`department`,`primaryaffiliation`) VALUES
+('1','1','t'),
+('2','1','t'),
+('3','1','f'),
+('3','2','t'),
+('4','1','t'),
+('5','1','t'),
+('6','2','t'),
+('7','1','f'),
+('7','2','t'),
+('8','1','t'),
+('9','3','t');
+
+/* Table structure for: procedure*/
+DROP TABLE IF EXISTS `procedure`;
+CREATE TABLE `procedure` (
+`code` INT PRIMARY KEY,
+`name` VARCHAR(50),
+`cost` REAL
+);
+INSERT INTO `procedure` (`code`,`name`,`cost`) VALUES
+(1,'Reverse Rhinopodoplasty',1500),
+(2,'Obtuse Pyloric Recombobulation',3750),
+(3,'Folded Demiophtalmectomy',4500),
+(4,'Complete Walletectomy',10000),
+(5,'Obfuscated Dermogastrotomy',4899),
+(6,'Reversible Pancreomyoplasty',5600),
+(7,'Follicular Demiectomy', 25);
+
+
+/* Table structure for: trained_in*/
+DROP TABLE IF EXISTS `trained_in`;
+CREATE TABLE `trained_in` (
+`physician` INT,
+`treatment` INT,
+`certificationdate` DATE,
+`certificationexpires` DATE,
+PRIMARY KEY (`physician`, `treatment`)
+);
+INSERT INTO `trained_in`(`physician`,`treatment`,`certificationdate`,`certificationexpires`) VALUES
+(3,1,'2008-01-01','2008-12-31'),
+(3,2,'2008-01-01','2008-12-31'),
+(3,5,'2008-01-01','2008-12-31'),
+(3,6,'2008-01-01','2008-12-31'),
+(3,7,'2008-01-01','2008-12-31'),
+(6,2,'2008-01-01','2008-12-31'),
+(6,5,'2007-01-01','2007-12-31'),
+(6,6,'2008-01-01','2008-12-31'),
+(7,1,'2008-01-01','2008-12-31'),
+(7,2,'2008-01-01','2008-12-31'),
+(7,3,'2008-01-01','2008-12-31'),
+(7,4,'2008-01-01','2008-12-31'),
+(7,5,'2008-01-01','2008-12-31'),
+(7,6,'2008-01-01','2008-12-31'),
+(7,7,'2008-01-01','2008-12-31');
+
+/*Table structure for table: patient*/
+DROP TABLE IF EXISTS `patient`;
+CREATE TABLE `patient` (
+`ssn` INT PRIMARY KEY,
+`name` varchar(40),
+`address` varchar(50),
+`phone` varchar(10),
+`insuranceid` INT,
+`pcp` INT
+);
+INSERT INTO `patient` (`ssn`,`name`,`address`,`phone`, `insuranceid`,`pcp`) VALUES
+(100000001,'John Smith','42 Foobar Lane','555-0256',68476213,1),
+(100000002,'Grace Ritchie','37 Snafu Drive','555-0512',36546321,2),
+(100000003,'Random J. Patient','101 Omgbbq Street','555-1204',65465421,2),
+(100000004,'Dennis Doe','1100 Foobaz Avenue','555-2048',68421879,3);
+
+/*Table structure for table: prescribes*/
+DROP TABLE IF EXISTS `prescribes`;
+CREATE TABLE `prescribes` (
+`physician` INT,
+`patient` INT,
+`medication` INT,
+`date` DATETIME,
+`appointment` INT,
+`dose` VARCHAR(2),
+PRIMARY KEY (`physician`,`patient`,`medication`,`date`)
+);
+INSERT INTO `prescribes` (`physician`,`patient`,`medication`,`date`,`appointment`,`dose`) VALUES
+(1,100000001,1,'2008-04-24 10:47:00',13216584,'5'),
+(9,100000004,2,'2008-04-27 10:53:00',86213939,'10'),
+(9,100000004,2,'2008-04-30 16:53:00',null,'5');
+
+/*Table structure for: medication*/
+DROP TABLE IF EXISTS `medication`;
+CREATE TABLE `medication` (
+code INT PRIMARY KEY,
+name varchar(50),
+brand varchar(50),
+description varchar(5)
+);
+INSERT INTO `medication`(code,name,brand, description) VALUES
+(1,'Procrastin-X','X','N/A'),
+(2,'Thesisin','Foo Labs','N/A'),
+(3,'Awakin','Bar Laboratories','N/A'),
+(4,'Crescavitin','Baz Industries','N/A'),
+(5,'Melioraurin','Snafu Pharmaceuticals','N/A');
+
+/*Table structure for: stay*/
+DROP TABLE IF EXISTS `stay`;
+CREATE TABLE `stay` (
+stayid INT PRIMARY KEY,
+patient INT,
+room INT,
+start_time DATETIME,
+end_time DATETIME
+);
+INSERT INTO `stay`(stayid,patient,room,start_time,end_time) VALUES
+(3215,100000001,111,'2008-05-01 00:00:00','2008-05-04 00:00:00'),
+(3216,100000003,123,'2008-05-03 00:00:00','2008-05-14 00:00:00'),
+(3217,100000004,112,'2008-05-02 00:00:00','2008-05-03 00:00:00');
+
+
+/*Table structure for: on_call*/
+DROP TABLE IF EXISTS `on_call`;
+CREATE TABLE `on_call` (
+`nurse` INT,
+`blockfloor` INT,
+`blockcode` INT,
+`oncallstart` DATETIME,
+`oncallend` DATETIME,
+PRIMARY KEY(`nurse`,`blockfloor`,`blockcode`,`oncallstart`,`oncallend`)
+);
+INSERT INTO on_call (`nurse`,`blockfloor`,`blockcode`,`oncallstart`,`oncallend`) VALUES
+(101,1,1,'2008-11-04 11:00:00','2008-11-04 19:00:00'),
+(101,1,2,'2008-11-04 11:00:00','2008-11-04 19:00:00'),
+(102,1,3,'2008-11-04 11:00:00','2008-11-04 19:00:00'),
+(103,1,1,'2008-11-04 19:00:00','2008-11-05 03:00:00'),
+(103,1,2,'2008-11-04 19:00:00','2008-11-05 03:00:00'),
+(103,1,3,'2008-11-04 19:00:00','2008-11-05 03:00:00');
+
+/*Table structure for: undergoes*/
+DROP TABLE IF EXISTS `undergoes`;
+CREATE TABLE `undergoes`(
+`patient` INT,
+`procedure` INT,
+`stay` INT,
+`date` DATETIME,
+`physician` INT,
+`assistingnurse` INT,
+PRIMARY KEY (`patient`,`procedure`,`stay`,`date`)
+);
+INSERT INTO `undergoes`(patient,`procedure`,`stay`,`date`,physician,assistingnurse) VALUES
+(100000001,6,3215,'2008-05-02 00:00:00',3,101),
+(100000001,2,3215,'2008-05-03 00:00:00',7,101),
+(100000004,1,3217,'2008-05-07 00:00:00',3,102),
+(100000004,5,3217,'2008-05-09 00:00:00',6,null),
+(100000001,7,3217,'2008-05-10 00:00:00',7,101),
+(100000004,4,3217,'2008-05-13 00:00:00',3,103);
+
+
+
+
+ALTER TABLE `on_call` 
+ADD CONSTRAINT `FK_oncall_nurse_employeeid` FOREIGN KEY (`nurse`) REFERENCES `nurse`(`employeeid`);
+
+ALTER TABLE `appointment` 
+ADD CONSTRAINT `FK_appointment_nurse_employeeid` FOREIGN KEY (`prepnurse`) REFERENCES `nurse`(`employeeid`),
+ADD CONSTRAINT `FK_appointment_patient_ssn` FOREIGN KEY (`patient`) REFERENCES `patient`(`ssn`),
+ADD CONSTRAINT `FK_appointment_physician_employeeid` FOREIGN KEY (`physician`) REFERENCES `physician`(`employeeid`);
+
+
+ALTER TABLE `undergoes` 
+ADD CONSTRAINT `FK_undergoes_nurse_employeeid` FOREIGN KEY (`assistingnurse`) REFERENCES `nurse`(`employeeid`),
+ADD CONSTRAINT `FK_undergoes_stay_stayid` FOREIGN KEY (`stay`) REFERENCES `stay`(`stayid`),
+ADD CONSTRAINT `FK_undergoes_procedure_code` FOREIGN KEY (`procedure`) REFERENCES `procedure`(`code`),
+ADD CONSTRAINT `FK_undergoes_patient_ssn` FOREIGN KEY (`patient`) REFERENCES `patient`(`ssn`),
+ADD CONSTRAINT `FK_undergoes_physician_employeeid` FOREIGN KEY (`physician`) REFERENCES `physician`(`employeeid`);
+
+
+ALTER TABLE `stay` 
+ADD CONSTRAINT `FK_stay_patient_ssn` FOREIGN KEY (`patient`) REFERENCES `patient`(`ssn`),
+ADD CONSTRAINT `FK_stay_room_roomnumber` FOREIGN KEY (`room`) REFERENCES `room`(`roomnumber`);
+
+ALTER TABLE `trained_in`
+ADD CONSTRAINT `FK_trained_in_procedure_code` FOREIGN KEY (`treatment`) REFERENCES `procedure`(`code`),
+ADD CONSTRAINT `FK_trained_in_physician_employeeid` FOREIGN KEY (`physician`) REFERENCES `physician`(`employeeid`);
+
+ALTER TABLE `prescribes` 
+ADD CONSTRAINT `FK_prescribes_appointment_patient` FOREIGN KEY (`patient`) REFERENCES `appointment`(`patient`),
+ADD CONSTRAINT `FK_prescribes_patient_ssn` FOREIGN KEY (`patient`) REFERENCES `patient`(`ssn`),
+ADD CONSTRAINT `FK_prescribes_medication_code` FOREIGN KEY (`medication`) REFERENCES `medication`(`code`),
+ADD CONSTRAINT `FK_prescribes_physician_employeeid` FOREIGN KEY (`physician`) REFERENCES `physician`(`employeeid`);
+
+
+ALTER TABLE `affiliated_with`
+ADD CONSTRAINT `FK_affiliated_department_departmentid` FOREIGN KEY (`department`) REFERENCES `department`(`departmentid`),
+ADD CONSTRAINT `FK_affiliated_physician_employeeid` FOREIGN KEY (`physician`) REFERENCES `physician`(`employeeid`);
+
+ALTER TABLE `patient`
+ADD CONSTRAINT `FK_patient_physician_employeeid` FOREIGN KEY (`pcp`) REFERENCES `physician`(`employeeid`);
+
+ALTER TABLE `department`
+ADD CONSTRAINT `FK_department_physician_employeeid` FOREIGN KEY (`head`) REFERENCES `physician`(`employeeid`);
+
+
